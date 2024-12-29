0f4597bfdab98d23c46654ce245b072170483d44
mdkawsar-alam <mdkawsaralam537@gmail.com>
2024-12-29 17:08:25 +0600

    todo list all file

--- index.js
+++ index.js
@@ -25,8 +25,11 @@
     li.textContent=task;
 
    const deleteBtn = document.createElement('button');
+
     deleteBtn.textContent='remove';
-    deleteBtn.classList.add('delete-btn')
+
+    deleteBtn.classList.add('delete-btn');
+
      deleteBtn.addEventListener('click', ()=>li.remove() )
 
      li.addEventListener('click',()=>li.classList.toggle('completed'))
@@ -75,35 +78,4 @@
 
 
 
-
-// const form = document.getElementById("todo-form");
-// const input = document.getElementById("todo-input");
-// const  todoList = document.getElementById("todo-list");
-
-// //  form submit
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   const task = input.value.trim();
-
-//   if (task !== "") {
-//     addtask(task);
-//     input.value = "";
-//   }
-// });
-
-// const addtask = (task) => {
-//   const li = document.createElement("li");
-//   li.textContent = task;
-
-//   const deletebtn= document.createElement('button');
-//   deletebtn.textContent='delete'
-//   deletebtn.classList.add("delete-btn");
-//   deletebtn.addEventListener('click',()=>li.remove())
-
-
-
-//   li.addEventListener('click', () => li.classList.toggle('completed'));
-//   li.appendChild(deletebtn)
-//    todoList.appendChild(li)
-// };
 
